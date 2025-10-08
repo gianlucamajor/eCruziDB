a021b0675389d96d82a54e2295d0b656d3c8329f
Gianluca <gianlucamajor@gmail.com>
2025-10-08 15:18:14 -0300

    filtered by protein description

--- web-app/src/components/Table.tsx
+++ web-app/src/components/Table.tsx
@@ -44,9 +44,7 @@ function Table() {
     (epitope) =>
       (epitope.Epitope ?? "").toLowerCase().includes(search.toLowerCase()) ||
       (epitope.ID ?? "").toLowerCase() === search.toLowerCase() ||
-      (epitope.Features?.GenomicRegionsAnnotation?.some(a =>
-        a.description?.toLowerCase().includes(search.toLowerCase())
-      ) ?? false) ||
+      (epitope.Features?.ProteinBestHit?.protein_description?.toLowerCase().includes(search.toLowerCase()) ?? false) ||
       (epitope.Features?.tcIEDB?.some(i =>
         i.sequence?.toLowerCase().includes(search.toLowerCase())
       ) ?? false)
